42f2c2e5915751a37ac25fdebb68137301e98ffa
nullwill <66563045+nullwill@users.noreply.github.com>
2025-06-27 22:42:15 +0000

    Renumbering Files

---
+++ organize/renumberFiles.py
@@ -0,0 +1,4 @@
+# rememberFiles.py - Find all files with a given prefix, such as spam001.txt,
+# spam002.txt, and so on, in a single folder and locates any gaps in the numbering
+# (such as if there is a spam001.txt and a spam003.txt but no spam002.txt). The
+# program rename all later files to close the gap.
